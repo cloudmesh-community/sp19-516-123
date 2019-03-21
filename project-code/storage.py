2df62f0c1996ecb88a2e77070e36b4694d119f88
mallik3006 <44556708+mallik3006@users.noreply.github.com>
2019-03-21 00:08:24 -0500

    Update storage.py

--- project-code/storage.py
+++ project-code/storage.py
@@ -1,25 +1,39 @@
+from __future__ import print_function
+from cloudmesh.shell.command import command
+from cloudmesh.shell.command import PluginCommand
+from cloudmesh.storage.api.manager import Manager
+from cloudmesh.shell.variables import Variables
+from pprint import pprint
+from cloudmesh.common.console import Console
+from cloudmesh.common.parameter import Parameter
+
+
+# noinspection PyBroadException
+class StorageCommand(PluginCommand):
+
+    # noinspection PyUnusedLocal
+    @command
+    def do_storage(self, args, arguments):
         """
         ::
 
           Usage:
-                storage [--storage=SERVICE] put CONTAINER FILENAME SOURCEDIR
-                storage [--storage=SERVICE] get CONTAINER FILENAME DESTDIR
-                storage [--storage=SERVICE] delete file CONTAINER FILENAME
-                storage [--storage=SERVICE] list file CONTAINER
-                storage [--storage=SERVICE] info CONTAINER FILENAME
-                storage [--storage=SERVICE] create file CONTAINER FILENAME
-                storage [--storage=SERVICE] create dir CONTAINER
+                storage [--storage=SERVICE] put FILENAME SOURCEDIR
+                storage [--storage=SERVICE] get FILENAME DESTDIR
+                storage [--storage=SERVICE] delete file FILENAME
+                storage [--storage=SERVICE] list file DIRNAME
+                storage [--storage=SERVICE] info FILENAME
+                storage [--storage=SERVICE] create dir DIRNAME
                 storage [--storage=SERVICE] list dir
-                storage [--storage=SERVICE] delete dir CONTAINER
+                storage [--storage=SERVICE] delete dir DIRNAME
 
 
           This command does some useful things.
 
           Arguments:
-              CONTAINER  a CONTAINER name
               FILENAME   a BLOB name
-              SOURCEDIR  local download path for FILENAME 
-              DESTDIR    local upload path for the FILENAME
+              SOURCEDIR  local path for the FILENAME to be uploaded
+              DESTDIR    local path for the FILENAME to be downloaded
 
           Options:
               --storage=SERVICE  specify the cloud service name like aws or azure or box or google
@@ -28,41 +42,105 @@
                 commands used to upload, download, list files on different cloud storage services.
 
                 storage put [options..]
-                    Uploads the file specified in the filename to the container name specified
-                    on the specified cloud  from the SOURCEDIR.
+                    Uploads the file specified in the filename to specified cloud from the SOURCEDIR.
 
                 storage get [options..]
-                    Downloads the file specified in the filename from the container name specified
-                    on the specified cloud to the DESTDIR.
+                    Downloads the file specified in the filename from the specified cloud to the DESTDIR.
 
                 storage delete file [options..]
-                    Deletes the file specified in the filename from the container name specified
-                    on the specified cloud.
+                    Deletes the file specified in the filename from the specified cloud.
 
                 storage list file [options..]
                     lists all the files from the container name specified on the specified cloud.
 
                 storage info [options..]
-                    returns the properties of the filename specified from the container name specified
-                    on the specified cloud.
+                    returns the properties of the filename specified on the specified cloud.
 
                 storage create dir [options..]
-                    creates the container with the container name specified on the specified cloud.
+                    creates a folder with the directory name specified on the specified cloud.
 
                 storage list dir [options..]
-                    lists all the containers on the specified cloud.
+                    lists all the folders on the specified cloud.
 
                 storage delete dir [options..]
-                    deletes the container with the container name specified on the specified cloud.
+                    deletes all the files in the directory specified on the specified cloud.
 
 
           Example:
-            set storage=azure
-            storage put CONTAINER FILENAME
+            set storage=azureblob
+            storage put FILENAME SOURCEDIR
 
             is the same as 
 
-            storage --storage=azure put CONTAINER FILENAME
+            storage --storage=azureblob put FILENAME SOURCEDIR
 
 
         """
+        # arguments.CONTAINER = arguments["--container"]
+        arguments.SERVICE = arguments["--storage"]
+        pprint(arguments)
+
+        m = Manager()
+
+        service = None
+
+        try:
+            service = arguments["--storage"][0]
+        except Exception as e:
+            try:
+                v = Variables()
+                service = v['storage']
+            except Exception as e:
+                service = None
+
+        if service is None:
+            Console.error("storage service not defined")
+
+        if arguments['get']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.get(arguments.SERVICE, arguments.FILENAME, arguments.DESTDIR)
+
+        elif arguments['put']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.put(arguments.SERVICE, arguments.FILENAME, arguments.SOURCEDIR)
+
+        elif arguments['delete'] and  arguments['file']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.delete(arguments.SERVICE, arguments.FILENAME)
+
+        elif arguments['list'] and arguments['file']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.listfiles(arguments.SERVICE, arguments.DIRNAME)
+
+        elif arguments['info']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.info(arguments.SERVICE, arguments.FILENAME)
+
+        elif arguments['create'] and arguments['dir']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.createdir(arguments.SERVICE, arguments.DIRNAME)
+
+        elif arguments['list'] and arguments['dir']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.listdir(arguments.SERVICE)
+
+        elif arguments['delete'] and arguments['dir']:
+            if arguments.SERVICE is None:
+                variables = Variables()
+                arguments.SERVICE = variables['storage']
+            m.deletedir(arguments.SERVICE, arguments.DIRNAME)
+
